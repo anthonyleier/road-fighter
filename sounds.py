256fcaa096a3c92e5a09d527f69623a9cfa08afc
Anthony Cruz <Anthonyleierlw@gmail.com>
2022-12-29 15:28:37 -0300

    Comments and filenames for sounds

--- sounds.py
+++ sounds.py
@@ -6,3 +6,15 @@ collectFuelSound = pygame.mixer.Sound('sounds/fuel.wav')
 endingMusic = pygame.mixer.Sound('sounds/game_over.wav')
 emptyFuelSound = pygame.mixer.Sound('sounds/empty_gas.wav')
 startingMusic = pygame.mixer.Sound('sounds/game_start.wav')
+motorIdle = pygame.mixer.Sound('sounds/motor_player.wav')
+motorAccelerated = pygame.mixer.Sound('sounds/motor_player_accelerated.wav')
+
+
+def stopAllSounds():
+    explosionSound.stop()
+    collectFuelSound.stop()
+    endingMusic.stop()
+    emptyFuelSound.stop()
+    startingMusic.stop()
+    motorIdle.stop()
+    motorAccelerated.stop()
